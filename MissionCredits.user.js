e32fb366e7eb838f60e468d0746337a3d65e1ac7
JRH-1997 <65117490+JRH-1997@users.noreply.github.com>
2020-05-24 16:54:42 +0200

    Update MissionCredits.user.js

--- MissionCredits.user.js
+++ MissionCredits.user.js
@@ -75,7 +75,7 @@
                 var html_str = '';
 
                 if (get_credits_for_type(Missions[i].getAttribute('mission_type_id')) == "Alleen Ambulance") html_str = 'Alleen Ambulance';
-                elseif (get_credits_for_type(Missions[i].getAttribute('mission_type_id')) != "Alleen Ambulance") html_str = 'Gem. ' + get_credits_for_type(Missions[i].getAttribute('mission_type_id')) + ' Credits';
+                if (get_credits_for_type(Missions[i].getAttribute('mission_type_id')) != "Alleen Ambulance") html_str = 'Gem. ' + get_credits_for_type(Missions[i].getAttribute('mission_type_id')) + ' Credits';
                 else (get_credits_for_type(Missions[i].getAttribute('mission_type_id')) == "Alleen Ambulance") html_str = 'Alleen Ambulance';
 
                 div_elem.innerHTML = html_str;
@@ -112,7 +112,7 @@
 
             // create div element
             if (credits == "Alleen Ambulance") html_str = 'Alleen Ambulance';
-            elseif (Credits != 'Alleen Ambulance") html_str = 'Gem. ' + credits + ' Credits';
+            if (Credits != 'Alleen Ambulance") html_str = 'Gem. ' + credits + ' Credits';
             else  (credits == "Alleen Ambulance") html_str = 'Alleen Ambulance';
                     
             var div_elem = document.createElement('div');
