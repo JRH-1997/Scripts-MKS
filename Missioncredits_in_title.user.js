d377a49486a3999ea6442bf6f14fc2ffce3cd91d
JRH-1997 <65117490+JRH-1997@users.noreply.github.com>
2020-06-10 16:55:22 +0200

    Layout update

--- Missioncredits_in_title.user.js
+++ Missioncredits_in_title.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Missioncredits in title
 // @namespace    http://tampermonkey.net/
-// @version      0.1.2
+// @version      0.1.3
 // @description  NL Credits in Missiontitle
 // @author       JRH1997
 // @match        https://www.meldkamerspel.com/*
@@ -37,7 +37,7 @@ async function init()
 
         let missionH1 = $("#missionH1");
 
-        let html = `
+        let html = `<br>
 		<span class="label label-primary"> <span id='html_str'> - </span></span>
 		`;
 
