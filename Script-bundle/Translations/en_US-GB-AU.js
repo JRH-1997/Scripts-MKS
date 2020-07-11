434bf0b8e4f58699a30eafaf21b9b4c54d513c28
JRH-1997 <65117490+JRH-1997@users.noreply.github.com>
2020-07-12 00:48:55 +0200

    Create en_US-GB-AU.js

---
+++ Script-bundle/Translations/en_US-GB-AU.js
@@ -0,0 +1,77 @@
+  // General Menu Translations
+    var SettingsScripts="Settings Script";
+    var On="On: ";
+    var Off="Off: ";
+
+    // Main menu Translation scripts
+    var Credits_Menu_Translation="Credits";
+    var Credits_Title_Menu_Translation="Shows the average credits to be earned";
+    var AllianceLabel_Menu_Translation="Alliance label";
+    var AllianceLabel_Title_Menu_Translation="Show a label with 'Alliance' from an adjustable value of credits";
+    var HideAlarmNextShare_Menu_Translation="Hide Alarm Next <br>Share button";
+    var HideAlarmNextShare_Title_Menu_Translation="Hide the Alarm Next Share Buttons";
+    var HideAlarmNextShare_onlyupper_Menu_Translation="Only the upper button";
+    var HideAlarmNextShare_onlyupper_Title_Menu_Translation="Hide Only the upper Alarm Next Share Button";
+    var Hotkey_Menu_Translation="Hotkeys";
+    var HideMap_Menu_Translation="Hide on Map";
+    var HideMissionlist_Menu_Translation="Hide in the missionlist";
+
+
+    // Side menu Credits Translations
+    var Missionlistlabel_Menu_Translation="In missionlist as label";
+    var Missionlisttext_Menu_Translation="In missionlist as text";
+    var Missionlistlabelafter_Menu_Translation="In missionlist as label<br>after the name";
+    var Missionlisttextafter_Menu_Translation="In missionlist as text<br>after the name";
+    var Missionlistlabelafter_Title_Menu_Translation="In missionlist as label after the name";
+    var Missionlisttextafter_Title_Menu_Translation="In missionlist as text after the name";
+    var Missionheaderlabel_Menu_Translation="In missionheader as label";
+    var Missionheadertext_Menu_Translation="In missionheader as text";
+    var Missionlistlabel_Title_Menu_Translation="In missionlist as label";
+    var Missionlisttext_Title_Menu_Translation="In missionlist as text";
+    var Missionheaderlabel_Title_Menu_Translation="In missionheader as label";
+    var Missionheadertext_Title_Menu_Translation="In missionheader as text";
+    var RemainingCredits_Menu_Translation="Totals above missionlist";
+    var RemainingCreditsTotal_Menu_Translation="Grand total after totals";
+    var RemainingCredits_Title_Menu_Translation="Shows the totals of the missions in categories: "+"Own missions / Planned missions / Alliance Missions";
+    var RemainingCreditsTotal_Title_Menu_Translation="Shows a grand total after the totals: "+"Own missions / Planned missions / Alliance Missions / Grand total";
+
+    // Side Menu AllianceLabel Translations
+    var AllianceLabel_missionheader_Menu_Translation="In missionheader";
+    var AllianceLabel_missionlist_Menu_Translation="In missionlist";
+    var AllianceLabel_missionheader_Title_Menu_Translation="In missionheader";
+    var AllianceLabel_missionlist_Title_Menu_Translation="In missionlist";
+    var AllianceLabelValue_setting_Translation="Label 'Alliance' from:";
+    var AllianceLabelValue_setting_save_Translation="Save setting";
+    // Side Menu Hotkey Translations
+    var Hotkey_SearchKeycode_Menu_Translation="Find the code for a shortcut here";
+    var Hotkey_InputKeycode_Menu_Translation="Enter the desired code below:";
+    var Hotkey_OpenTransportRequest_Menu_Translation="Open Transport request (Q)";
+    var Hotkey_SavePOI_Menu_Translation="Save new POI (space)";
+    // Side Menu HideMap Translations
+    var HideMap_AllOwnBuildings_Menu_Translation="All own buildings";
+    var HideMap_OwnFireBuidlings_Menu_Translation="All own fire buildings";
+    var HideMap_OwnAmbulanceBuildings_Menu_Translation="All own ambulance buildings";
+    var HideMap_OwnPoliceBuildings_Menu_Translation="All own police buildings";
+    var HideMap_AllAllianceBuildings_Menu_Translation="All alliance buildings";
+    var HideMap_AllMissionIcons_Menu_Translation="All missions";
+    var HideMap_AllAllianceMissions_Menu_Translation="All alliance missions";
+    var HideMap_AllOwnMissions_Menu_Translation="All own missions";
+    var HideMap_AllEventMissions_Menu_Translation="All event missions";
+    var HideMap_AllOwnVehicles_Menu_Translation="All own vehicles";
+    var HideMap_AllAllianceVehicles_Menu_Translation="All alliance vehicles";
+    var HideMap_AllOwnNotInvolvedVehicles_Menu_Translation="All own not involved vehicles";
+    // Side Menu HideMissionlist Translation
+    var HideMissionlist_AllOwnMissions_Menu_Translation="All own missions";
+    var HideMissionlist_InvolvedOwnMissions_Menu_Translation="Involved own missions";
+    var HideMissionlist_SharedOwnMissions_Menu_Translation="Shared own missions";
+    var HideMissionlist_NotSharedOwnMissions_Menu_Translation="Not shared own missions";
+    var HideMissionlist_AllAllianceMissions_Menu_Translation="All alliance missions";
+    var HideMissionlist_InvolvedAllianceMissions_Menu_Translation="Involved alliance missions";
+    var HideMissionlist_InvolvedEventMissions_Menu_Translation="Involved event missions";
+    }// end menu translations
+
+    /* Script Translations */{
+    // Script Credits Missionlist/Missionheader/RemainingCredits/Alliancelabel Translations
+    var OnlyAmbulance="Only Ambulance";
+    var ToEarn="To earn: ";
+    var LabelAlliance="ALLIANCE";
